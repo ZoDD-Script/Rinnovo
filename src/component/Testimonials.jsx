9fe8aac4b1a0082c6e5bd20bb59ffc1f9849ebb9
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-01-12 12:49:10 +0100

    fix

--- src/component/Testimonials.jsx
+++ src/component/Testimonials.jsx
@@ -1,6 +1,6 @@
+import FeedBackCard from './FeedBackCard'
 import { feedback } from '../constants'
 import styles from '../style'
-import FeedBackCard from './FeedBackCard'
 
 const Testimonials = () => (
   <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
