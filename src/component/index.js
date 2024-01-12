ec6e96fddd2ffacdf885e4cfc7c7f17142fab7f6
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-01-12 12:38:41 +0100

    removing cta

--- src/component/index.js
+++ src/component/index.js
@@ -3,7 +3,7 @@ import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Business from "./Business";
 import Clients from "./Clients";
-import CTA from "./Cta";
+// import CTA from "./CTA";
 import Stats from "./Stats";
 import Footer from "./Footer";
 import Hero from "./Hero";
@@ -15,7 +15,7 @@ export {
   CardDeal,
   Business,
   Clients,
-  CTA,
+  // CTA,
   Stats,
   Testimonials,
   Footer,
