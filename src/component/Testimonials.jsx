6189c80ad0a9b41ad9bb1278fcd6347c2d911753
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-01-12 13:02:58 +0100

    again

--- src/component/Testimonials.jsx
+++ src/component/Testimonials.jsx
@@ -1,6 +1,22 @@
-import FeedBackCard from './FeedBackCard'
+// import FeedBackCard from './FeedBackCard'
 import { feedback } from '../constants'
 import styles from '../style'
+import { quotes } from '../assets'
+
+const FeedbackCard = ({ content, name, title, img }) =>  (
+  <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+    <img src={quotes} alt="double_quotes" className='w-[42px] h-[27px] object-contain' />
+    <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'>{content}</p>
+
+    <div className='flex flex-row'>
+      <img src={img} alt={name} className='w-[48px] h-[48px] rounded-full' />
+      <div className='flex flex-col ml-4'>
+        <h4 className='font-poppins font-semibold text-[20px] leading-[32px] text-white '>{name}</h4>
+        <p className='font-poppins font-normal text-[16px] leading-[24px] text-dimWhite '>{title}</p>
+      </div>
+    </div>
+  </div>
+)
 
 const Testimonials = () => (
   <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
@@ -16,7 +32,7 @@ const Testimonials = () => (
 
     <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
       {feedback.map((card) => (
-        <FeedBackCard key={card.id} {...card} />
+        <FeedbackCard key={card.id} {...card} />
       ))}
     </div>
   </section>
