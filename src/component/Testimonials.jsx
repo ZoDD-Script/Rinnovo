1bb4bc68564a268e6427c0c45d9801d1913919ad
Chukwu Blessed <blessedchukwu89@gmail.com>
2024-01-12 12:51:51 +0100

    temporaly removing feedback card

--- src/component/Testimonials.jsx
+++ src/component/Testimonials.jsx
@@ -15,9 +15,9 @@ const Testimonials = () => (
     </div>
 
     <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
-      {feedback.map((card) => (
+      {/* {feedback.map((card) => (
         <FeedBackCard key={card.id} {...card} />
-      ))}
+      ))} */}
     </div>
   </section>
 )
